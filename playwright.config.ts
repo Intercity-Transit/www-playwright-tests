66817756f07af3ae1b8bce6d33970f60e3154f09
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2025-09-13 15:26:58 -0700

    Workflow schedule updates.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './src/tests',
   reporter: [['list'], ['html', { outputFolder: 'test-results', open: 'never' }]],
   use: {
-    baseURL: process.env.BASE_URL || 'https://dev-intercity-transit.pantheonsite.io/',
+    baseURL: process.env.BASE_URL || 'https://test-intercity-transit.pantheonsite.io/',
     headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
